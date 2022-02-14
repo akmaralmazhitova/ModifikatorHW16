9fbe0eb7bb9cea3b3b1e9637d424b26425b5f6f0
akmaralmazhitova <akmaral.mazhitova.kk@gmail.com>
2022-02-14 15:17:40 +0600

    ondoldu

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -3,14 +3,14 @@ package com.company;
 import java.util.Scanner;
 
 public class Circle {
-    static final double PI = 3.14;
-    static final double radius = 2.2;
+     final double PI = 3.14;
+     final double radius = 2.2;
 
-    static void area(){
+    public void area(){
         double area = PI * (radius * radius);
         System.out.println(area);
     }
-    static void circumference(){
+    public void circumference(){
         double circum = PI * 2 * radius;
         System.out.println(circum);
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Circle circle = new Circle();
-        Circle.area();
-        Circle.circumference();
+        circle.area();
+        circle.circumference();
     }
 }
